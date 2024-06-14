5f0aa1b6503b3f786b44448e7861c6e7b49828ed
poojitha427742 <111869733+poojitha427742@users.noreply.github.com>
2024-06-14 10:53:12 +0530

    Add files via upload
    password generator

---
+++ password generator.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+def generate_password(length):
+    
+    characters = string.ascii_letters + string.digits + string.punctuation
+   
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def main():
+   
+    while True:
+        try:
+            length = int(input("Enter the desired length of the password: "))
+            if length <= 0:
+                print("Please enter a positive integer.")
+            else:
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+    
+   
+    password = generate_password(length)
+    
+    
+    print(f"Generated Password: {password}")
+
+if __name__ == "__main__":
+    main()
